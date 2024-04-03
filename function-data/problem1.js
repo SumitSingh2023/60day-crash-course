d63383b2a7b781f9f9fd72fcabbff23a04cd6310
Sumit singh <sumit700490@gmail.com>
2024-04-03 11:31:47 +0530

    ok

---
+++ function-data/problem1.js
@@ -0,0 +1,20 @@
+// Problem 1: Create a function to check if a number is Prime or Not
+
+function checkprime(num){
+    
+    let factor=0
+    for(let i=0 ; i<=num;i++){
+        if(num%i==0){
+            factor++
+            
+        }
+        
+    }
+    if(factor==2){
+        console.log("Prime Number")
+    }else{
+        console.log("Not Prime Number")
+    }
+}
+
+checkprime(10)
